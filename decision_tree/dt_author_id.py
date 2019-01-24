7dd8442732b745add515e711b94adfa7f4358015
Christopher Ray <cray1@email.arizona.edu>
2019-01-23 23:31:13 -0800

    decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree 
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -28,4 +29,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
 
+prediction = clf.predict(features_test)
+
+accuracy = accuracy_score(labels_test, prediction)
+
+print "accuracy: "+ str(accuracy)
+print "no of features: "+ str(len(features_train[0]))
